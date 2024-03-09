97b3e7d508f1d2ac7603d5f7ffbf28c9a7913e17
Bugraunluu <bugraunl0607@gmail.com>
2024-03-10 01:07:59 +0300

    a

--- Assets/Scripts/Player/Player_HP.cs
+++ Assets/Scripts/Player/Player_HP.cs
@@ -5,28 +5,56 @@ using UnityEngine;
 public class Player_HP : MonoBehaviour
 {
     public Animator anim;
-    public float health;
+    public float currentHealth;
+    public float startingHealth;
     public float maxHealth;
-   // public Image healthBar;
+    private bool dead=false;
+    // public Image healthBar;
     public GameObject player;
     private float timer;
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = health;
+        maxHealth = currentHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         //healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
-        if (health <= 0)
+        if (currentHealth <= 0)
         {
             timer += Time.deltaTime;
             if (timer > 1)
             {
                 Destroy(player);
 
+            }
+        }
+    }
+    public void TakeDamage(float _damage)
+    {
+        
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
+
+                dead = true;
             }
         }
     }
